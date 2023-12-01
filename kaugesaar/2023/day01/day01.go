428c3c435f02a00c5837b785e34f8a03437414a3
Victor Kaugesaar <kaugesaar@precisdigital.com>
2023-12-01 11:42:33 +0100

    day1 🎄

--- kaugesaar/2023/day01/day01.go
+++ kaugesaar/2023/day01/day01.go
@@ -1,15 +1,103 @@
 package day01
 
-import "kaugesaar-aoc/common"
+import (
+	_ "embed"
+	"kaugesaar-aoc/common"
+	"kaugesaar-aoc/utils"
+	"math"
+	"strings"
+	"unicode"
+)
 
+//go:embed day1.txt
+var fileInput string
+
+// Solver for day 1 and its both parts
 type Solver struct{}
+
+func p2() string {
+	rows := strings.Split(fileInput, "\n")
+	var numbers []int
+
+	digitMap := map[string]int{
+		"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+		"six": 6, "seven": 7, "eight": 8, "nine": 9,
+	}
+
+	findNumber := func(word string, index int) (string, bool) {
+		start := int(math.Max(0, float64(index-1)))
+		for i := 1; i <= 5 && start+i <= len(word); i++ {
+			slicedWord := word[start : start+i]
+			if num, exsists := digitMap[slicedWord]; exsists {
+				return utils.ToStr(num), true
+			}
+		}
+		return "", false
+	}
+
+	for _, row := range rows {
+		var first, last string
+		foundFirst := false
+		currentWord := ""
+
+		for i, r := range row {
+			if unicode.IsDigit(r) {
+				if !foundFirst {
+					first = string(r)
+					foundFirst = true
+				}
+				last = string(r)
+			} else if unicode.IsLetter(r) {
+				currentWord += string(r)
+				if num, exsits := findNumber(row, i); exsits {
+					if !foundFirst {
+						first = num
+						foundFirst = true
+					}
+					last = num
+					currentWord = ""
+				}
+			}
+		}
+
+		number := utils.ToInt(first + last)
+		numbers = append(numbers, number)
+	}
+
+	return utils.ToStr(utils.SumArr(numbers))
+}
+
+func p1() string {
+	rows := strings.Split(fileInput, "\n")
+	var numbers []int
+
+	for _, row := range rows {
+		var first, last string
+		foundFirst := false
+
+		for _, r := range row {
+			if unicode.IsDigit(r) {
+				if !foundFirst {
+					first = string(r)
+					foundFirst = true
+				}
+				last = string(r)
+			}
+		}
+
+		number := utils.ToInt(first + last)
+		numbers = append(numbers, number)
+	}
+
+	return utils.ToStr(utils.SumArr(numbers))
+}
 
 // Part1 the solution for part 1, day 1
 func (s Solver) Part1() common.Solution {
 	return common.Solution{
 		Day:    1,
 		Part:   1,
-		Answer: "1111111111",
+		Answer: p1(),
 	}
 }
 
@@ -18,6 +106,6 @@ func (s Solver) Part2() common.Solution {
 	return common.Solution{
 		Day:    1,
 		Part:   2,
-		Answer: "2222222222",
+		Answer: p2(),
 	}
 }
--- kaugesaar/utils/utils.go
+++ kaugesaar/utils/utils.go
@@ -6,3 +6,15 @@ func ToInt(s string) int {
 	i, _ := strconv.Atoi(s)
 	return i
 }
+
+func ToStr(i int) string {
+	return strconv.Itoa(i)
+}
+
+func SumArr(arr []int) int {
+	sum := 0
+	for _, value := range arr {
+		sum += value
+	}
+	return sum
+}
