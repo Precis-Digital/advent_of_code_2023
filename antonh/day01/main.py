194a62aeea63a44ca4bb9feab15bd214714e4886
Anton Tikjøb Hagedorn <anton@precisdigital.com>
2023-12-01 14:36:44 +0100

    trying my best

---
+++ antonh/day01/main.py
@@ -0,0 +1,7 @@
+input_path = "Precis-Digital/advent_of_code_2023/antonh/inputs/day01.txt"
+
+def main(file_path):
+    with open(file_path) as file:
+        file_contents = file.read()
+        print(file_contents)
+
