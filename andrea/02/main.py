3d0e3021e8521846462c4209f5366dbafb9c2189
Andrea Lari <andrea.l@boosterboxdigital.com>
2023-12-02 09:54:25 +0100

    Day 02

--- andrea/02/main.py
+++ andrea/02/main.py
@@ -1,63 +1,63 @@
 import re
 
 
-def solve_01_1():
+def solve_02_01():
     print('*' * 20, 'Step 1')
-    re_digits = re.compile(r'(\d)')
-    with open('input.txt', 'r') as input_file:
+    bag = {'red': 12,
+           'green': 13,
+           'blue': 14}
+    re_game = re.compile(r'Game (\d+)[;:] (.+)')
+    re_extraction = re.compile(r'(\d+) (' + '|'.join(bag.keys()) + r')')
+    with (open('input.txt', 'r') as input_file):
         result = 0
-        for line in input_file:
-            digits = re_digits.findall(line)
+        for game_line in input_file:
             try:
-                result += int(digits[0]) * 10 + int(digits[-1])
+                game_id, extractions = re_game.findall(game_line)[0]
+                game_possible = True
+                for extraction in extractions.split('; '):
+                    for color_num, color in re_extraction.findall(extraction):
+                        if int(color_num) > bag[color]:
+                            game_possible = False
+                            break
+                    if not game_possible:
+                        break
+                if game_possible:
+                    result += int(game_id)
             except:
-                print(line)
+                print(game_line)
                 raise
-
         print('*' * 20, 'Step 1', result)
 
 
-def solve_01_2():
+def solve_02_02():
     print('*' * 20, 'Step 2')
-    eng_digits = ['zero',
-                  'one',
-                  'two',
-                  'three',
-                  'four',
-                  'five',
-                  'six',
-                  'seven',
-                  'eight',
-                  'nine']
-    dict_eng_digits = {}
-    for index, key in enumerate(eng_digits):
-        dict_eng_digits[key] = index
-        dict_eng_digits[str(index)] = index
-    re_first_digit = re.compile(r'(\d|' + '|'.join(eng_digits) + r')')
-    re_last_digit = re.compile(r'.*(\d|' + '|'.join(eng_digits) + r')')
+    colors = ['red',
+              'green',
+              'blue']
+    re_game = re.compile(r'Game (\d+)[;:] (.+)')
+    re_extraction = re.compile(r'(\d+) (' + '|'.join(colors) + r')')
     with (open('input.txt', 'r') as input_file):
         result = 0
-        for line in input_file:
+        for game_line in input_file:
             try:
-                digits = re_first_digit.findall(line)
-                single_re_result = (dict_eng_digits[digits[0]] * 10
-                                    + dict_eng_digits[digits[-1]])
-                split_re_result = (dict_eng_digits[re_first_digit.search(line)[1]] * 10
-                                   + dict_eng_digits[re_last_digit.match(line)[1]])
-                if single_re_result != split_re_result:
-                    print('', f'Fail Single RE {digits[0]},{digits[-1]} - {re_first_digit.search(line)[1]}'
-                              f',{re_last_digit.match(line)[1]}')
-                    print('', line)
-                result += split_re_result
+                bag = {color: 0 for color in colors}
+                game_id, extractions = re_game.findall(game_line)[0]
+                for extraction in extractions.split('; '):
+                    for color_num, color in re_extraction.findall(extraction):
+                        bag[color] = max(bag[color], int(color_num))
+                game_power = 1
+                for color in colors:
+                    game_power *= bag[color]
+                result += game_power
             except:
-                print(line)
+                print(game_line)
                 raise
         print('*' * 20, 'Step 2', result)
 
 
 if __name__ == '__main__':
-    solve_01_1()
-    solve_01_2()
+    solve_02_01()
+    solve_02_02()
 
 # Output
 # 
