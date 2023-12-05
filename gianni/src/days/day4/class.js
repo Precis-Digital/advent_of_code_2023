52d4826dd62f1457b10e55c4090aff95c662ea9b
gianni marsiglia <gianni.marsiglia@bannerboy.com>
2023-12-05 10:20:15 +0100

    turning day 4 into class

--- gianni/src/days/day4/class.js
+++ gianni/src/days/day4/class.js
@@ -2,104 +2,202 @@
 
 import chalk from 'chalk';
 
-export default class CardGame {
+export default class Solution {
   constructor(dataLines, isLoggingEnabled) {
     this.dataLines = dataLines;
+    this.cards = [];
     this.isLoggingEnabled = isLoggingEnabled;
     this.cardIndexes = [];
-    this.cards = [];
-    this.totalSum = 0;
+    this.totalPoints = 0;
+    this.totalScratchcards = 0;
+  }
+
+  // core method
+  calculateWinningCards() {
+    if (!Array.isArray(this.dataLines) || this.dataLines.length === 0) {
+      throw new Error('Invalid input: dataLines must be a non-empty array');
+    }
+
+    // Part-1
+    if (this.isLoggingEnabled) {
+      console.log('\ncards\n');
+    }
+    const cards = new Array(this.dataLines.length);
+    const cardIndexes = new Array(this.dataLines.length);
+
+    this.dataLines.forEach((cardLine, index) => {
+      const card = this.parseCardLine(cardLine);
+      cards[index] = card;
+      cardIndexes[index] = index + 1;
+      this.totalPoints += card.points;
+    });
+
+    this.cards = cards;
+    // this.cardIndexes = cardIndexes;
+
+    this.cardIndexes = cards.flatMap((card) => {
+      if (card.cardIndexes.length > 0) {
+        return [card.cardIndexes];
+      }
+      return [];
+    });
+
+
+    // Part-2
+
+    // Loop thru all indexes in this.cardIndexes
+    // For each indexGroup, loop thru all indexes
+    // For each index, get the corresponding cardObject from this.cards
+    // For each cardObject, get the cardIndexes
+    // Add the cardIndexes to this.cardIndexes
+
+    console.log('\ninitial card clones:\n\n', this.cardIndexes);
+
+    ///////////////////////
+    // PROBLEM HERE ///////
+    ///////////////////////
+
+    this.cardIndexes.forEach((indexGroup) => {
+      indexGroup.forEach((index) => {
+        const cardObject = this.cards[index - 1];
+        if (cardObject) {
+          const cardClonesIndexes = cardObject.cardIndexes;
+          if (cardClonesIndexes.length > 0 && cardIndexes.length > 0) {
+            this.cardIndexes = [...this.cardIndexes, [...cardClonesIndexes]];
+            // console.log(groupIndex, indexGroup, cardObject, this.cardIndexes);
+          }
+        }
+      });
+    });
+
+    ///////////////////////
+    ///////////////////////
+
+    console.log('\nafter cloning all: (something goes wrong here)\n\n', this.cardIndexes);
+
+    this.cardIndexes.unshift(cardIndexes); // Add the original cardIndexes to the beginning of this.cardIndexes
+
+    console.log('\nafter adding original card indexes:\n\n', this.cardIndexes);
+
+    const cardInstances = this.cardIndexes.reduce((acc, curr) => {
+      curr.forEach((index) => {
+        if (acc[index]) {
+          acc[index] += 1;
+        } else {
+          acc[index] = 1;
+        }
+      });
+      return acc;
+    }, {});
+
+    const totalSum = Object.values(cardInstances).reduce((a, b) => a + b, 0);
+
+    console.log('\nindex overview:\n\n', cardInstances, 'total:', totalSum); // Outputs: 24
+  }
+
+  // splitCardLine main method
+  splitCardLine(cardLine) {
+    const { cardIndexPart, winnerNumberPart, myNumberPart } = this.splitCardLineIntoParts(cardLine);
+    const index = this.extractIndexFromCardPart(cardIndexPart);
+    const winnerNumbers = this.extractNumbersFromNumberPart(winnerNumberPart);
+    const myNumbers = this.extractNumbersFromNumberPart(myNumberPart);
+
+    return {
+      index,
+      winnerNumbers,
+      myNumbers,
+    };
+  }
+
+  // splitCardLine sub methods
+  splitCardLineIntoParts(cardLine) {
+    const [cardIndexPart, numbersPart] = cardLine.split(': ');
+    const [winnerNumberPart, myNumberPart] = numbersPart.split(' | ');
+    return { cardIndexPart, winnerNumberPart, myNumberPart };
+  }
+
+  extractIndexFromCardPart(cardIndexPart) {
+    const indexArray = cardIndexPart.split(' ');
+    const index = Number(indexArray[indexArray.length - 1]);
+    return index;
+  }
+
+  extractNumbersFromNumberPart(numberPart) {
+    return numberPart.split(' ').filter(item => item !== '').map(Number);
+  }
+
+  // parseCardLine main method
+  parseCardLine(cardLine, isClone = false) {
+    const parsedData = this.splitCardLine(cardLine);
+    const matchingNumbers = this.getMatchingNumbers(parsedData.winnerNumbers, parsedData.myNumbers);
+    const points = this.calculatePoints(matchingNumbers);
+    const cardIndexes = this.generateCardIndexes(Number(parsedData.index), matchingNumbers.length);
+
+    if (this.isLoggingEnabled) {
+      const logProperties = {
+        matchingNumbers,
+        points,
+        isClone,
+      };
+      this.logCardLine(cardLine, logProperties);
+    }
+
+    return {
+      index: parsedData.index,
+      points,
+      cardIndexes,
+    };
+  }
+
+  // parseCardLine sub methods
+  getMatchingNumbers(winnerNumbers, myNumbers) {
+    const winnerNumbersSet = new Set(winnerNumbers);
+    return myNumbers.filter(num => winnerNumbersSet.has(num));
+  }
+
+  calculatePoints(matchingNumbers) {
+    return matchingNumbers.length > 0 ? 2 ** (matchingNumbers.length - 1) : 0;
   }
 
   generateCardIndexes(index, length) {
     return Array.from({ length }, (_, i) => i + index + 1);
   }
 
-  parseCardData(cardData) {
-    const [cardIndexPart, numbersPart] = cardData.split(': ');
-    const [, index] = cardIndexPart.split(' ');
-    const [winnerNumberPart, myNumberPart] = numbersPart.split(' | ');
-    const winnerNumbers = winnerNumberPart.split(' ').map(Number);
-    const myNumbers = myNumberPart.split(' ').map(Number);
-    const matchingNumbers = myNumbers.filter(num => winnerNumbers.includes(num));
-    const points = matchingNumbers.length > 0 ? 2 ** (matchingNumbers.length - 1) : 0;
-    const cardIndexes = this.generateCardIndexes(Number(index), matchingNumbers.length);
-
-    if (this.isLoggingEnabled) {
-      cardData = cardData.replace('Card', matchingNumbers.length > 0 ? chalk.green('Card') : chalk.red('Card'));
-      cardData = chalk.grey(cardData);
-
-      numbersPart.map(() => matchingNumbers.reduce((updatedData, num) => {
-        const regex = new RegExp(` ${num} `, 'g');
-        return chalk.grey(updatedData.replace(regex, ` ${chalk.green(num)} `));
-      }, cardData)).join('');
-
-      console.log(
-        cardData,
-        `${chalk.blue('points:')} ${chalk.yellow(points)}`,
-      );
+  // logCardLine main method
+  logCardLine(cardLine, logProperties) {
+    const updatedCardLine = this.updateCardLine(cardLine, logProperties.matchingNumbers);
+    if (logProperties.isClone) {
+      console.log(updatedCardLine);
+    } else {
+      console.log(updatedCardLine, `${chalk.blue('points:')} ${chalk.yellow(logProperties.points)}`);
     }
-
-    return {
-      index,
-      points,
-      cardIndexes,
-    };
   }
 
-  cloneCardsRecursively() {
-    this.cards = this.cards.map((card) => {
-      const cardsClones = card.clonedCards.map((cardClonesNumber) => {
-        const cardCloneLine = this.dataLines[cardClonesNumber - 1];
-        const cardCloneParsed = this.parseCardData(cardCloneLine);
-        return cardCloneParsed.cardsClones.length > 0 ? cardCloneParsed : null;
-      }).filter(cardCloneParsed => cardCloneParsed !== null);
-
-      if (card.clonedCards.length > 0) {
-        this.cardIndexes.push(card.clonedCards);
-        this.cloneCardsRecursively(cardsClones);
-      }
-
-      return card;
-    });
+  // logCardLine sub methods
+  updateCardLine(cardLine, matchingNumbers) {
+    const cardColor = matchingNumbers.length > 0 ? chalk.green : chalk.red;
+    let updatedCardLine = this.updateCardColor(cardLine, cardColor, matchingNumbers);
+    updatedCardLine = this.highlightMatchingNumbers(updatedCardLine, matchingNumbers);
+    return updatedCardLine;
   }
 
-  calculateWinningCards() {
-    if (!Array.isArray(this.dataLines) || this.dataLines.length === 0) {
-      throw new Error('Invalid input: dataLines must be a non-empty array');
+  updateCardColor(cardLine, cardColor, matchingNumbers) {
+    return matchingNumbers.length > 0
+      ? cardLine.replace('Card', cardColor('Card'))
+      : cardLine.replace(cardLine, cardColor(cardLine));
+  }
+
+  highlightMatchingNumbers(cardLine, matchingNumbers) {
+    if (matchingNumbers.length > 0) {
+      const replacements = new Map(matchingNumbers.map((num) => {
+        if (num >= 0 && num < 10) { // check if num is a single digit
+          return [`  ${num}`, `  ${chalk.green(`${num}`)}`];
+        }
+        return [` ${num}`, ` ${chalk.green(num)}`];
+
+      })); const regex = new RegExp(Array.from(replacements.keys()).join('|'), 'g');
+      return cardLine.replace(regex, match => replacements.get(match));
     }
-
-    this.dataLines.reduce((acc, line, index) => {
-      const card = this.parseCardData(line);
-      this.cards.push(card);
-      this.cardIndexes.push(index + 1);
-      this.totalSum += card.points;
-
-      return card;
-    }, 0);
-
-    this.cloneCardsRecursively();
-
-    const cardCounts = this.cardIndexes.flatMap(indexes => indexes)
-      .reduce((acc, index) => {
-        acc[index] = (acc[index] || 0) + 1;
-        return acc;
-      }, {});
-
-    const scratchCardStats = Object.entries(cardCounts).map(([index, count]) => ({ index: Number(index), count }));
-
-    const scratchcardsObject = {
-      scratchCardStats,
-      total: scratchCardStats.reduce((acc, { count }) => acc + count, 0),
-    };
-
-    if (this.isLoggingEnabled) {
-      console.log('\n', scratchcardsObject);
-    }
-
-    return {
-      part1: this.totalSum,
-      part2: scratchcardsObject,
-    };
+    return cardLine;
   }
 }
--- gianni/src/days/day4/solution.js
+++ gianni/src/days/day4/solution.js
@@ -13,6 +13,8 @@ import chalk from 'chalk';
 import { getRuntime, getFileSize, loadTextFile } from '../../utils/getters.js';
 import { log } from '../../utils/log.js';
 
+import Solution from './class.js';
+
 /* CONSTANTS
 ============================================================================= */
 
@@ -20,128 +22,128 @@ const DAY = 'day4';
 
 /* CORE FUNCTIONS
 ============================================================================= */
-function generatLineNumbers(index, length) {
-  const numbers = [];
-  for (let i = index + 1; i <= index + length; i++) {
-    numbers.push(i);
-  }
-  return numbers;
-}
+// function generatLineNumbers(index, length) {
+//   const numbers = [];
+//   for (let i = index + 1; i <= index + length; i++) {
+//     numbers.push(i);
+//   }
+//   return numbers;
+// }
 
-const parseCard = (cardLine, isLoggingEnabled) => {
-  const parts = cardLine.split(': ');
-  const index = parts[0].split(' ').filter(str => str.length).map(Number)[1];
-  const numbers = parts[1].split(' | ');
-  const winnerNumber = numbers[0].split(' ').filter(str => str.length).map(Number);
-  const myNumber = numbers[1].split(' ').filter(str => str.length).map(Number);
-  const matchingNumbers = myNumber.filter(num => winnerNumber.includes(num));
-  const points = matchingNumbers.length > 0 ? 2 ** (matchingNumbers.length - 1) : 0;
+// const parseCard = (cardLine, isLoggingEnabled) => {
+//   const parts = cardLine.split(': ');
+//   const index = parts[0].split(' ').filter(str => str.length).map(Number)[1];
+//   const numbers = parts[1].split(' | ');
+//   const winnerNumber = numbers[0].split(' ').filter(str => str.length).map(Number);
+//   const myNumber = numbers[1].split(' ').filter(str => str.length).map(Number);
+//   const matchingNumbers = myNumber.filter(num => winnerNumber.includes(num));
+//   const points = matchingNumbers.length > 0 ? 2 ** (matchingNumbers.length - 1) : 0;
 
-  // get all copies now it only goes down one level but needs to be recursive
-  const cardsClones = generatLineNumbers(index, matchingNumbers.length);
+//   // get all copies now it only goes down one level but needs to be recursive
+//   const cardsClones = generatLineNumbers(index, matchingNumbers.length);
 
-  if (isLoggingEnabled) {
-    cardLine = cardLine.replace('Card', matchingNumbers.length > 0 ? chalk.green('Card') : chalk.red('Card'));
-    cardLine = chalk.grey(cardLine);
+//   if (isLoggingEnabled) {
+//     cardLine = cardLine.replace('Card', matchingNumbers.length > 0 ? chalk.green('Card') : chalk.red('Card'));
+//     cardLine = chalk.grey(cardLine);
 
-    numbers.forEach(() => {
-      matchingNumbers.forEach((num) => {
-        const regex = new RegExp(` ${num} `, 'g');
-        cardLine = chalk.grey(cardLine.replace(regex, ` ${chalk.green(num)} `));
-      });
-    });
+//     numbers.forEach(() => {
+//       matchingNumbers.forEach((num) => {
+//         const regex = new RegExp(` ${num} `, 'g');
+//         cardLine = chalk.grey(cardLine.replace(regex, ` ${chalk.green(num)} `));
+//       });
+//     });
 
-    console.log(
-      cardLine,
-      `${chalk.blue('points:')} ${chalk.yellow(points)}`,
-    );
-  }
+//     console.log(
+//       cardLine,
+//       `${chalk.blue('points:')} ${chalk.yellow(points)}`,
+//     );
+//   }
 
-  return {
-    index,
-    // matchingNumbers,
-    points,
-    cardsClones,
-  };
-};
+//   return {
+//     index,
+//     // matchingNumbers,
+//     points,
+//     cardsClones,
+//   };
+// };
 
-const recursiveCloneCards = (cards, dataLines, CARD_INDEXES, isLoggingEnabled) => {
-  const clonesGroups = cards.map((card) => {
-    // console.log(card.cardsClones);
-    const cardsClones = [];
+// const recursiveCloneCards = (cards, dataLines, CARD_INDEXES, isLoggingEnabled) => {
+//   const clonesGroups = cards.map((card) => {
+//     // console.log(card.cardsClones);
+//     const cardsClones = [];
 
-    card.cardsClones.forEach((cardClonesNumber) => {
-      const cardCloneLine = dataLines[cardClonesNumber - 1];
-      const cardCloneParsed = parseCard(cardCloneLine, false);
-      if (cardCloneParsed.cardsClones.length > 0) {
-        cardsClones.push(cardCloneParsed);
-      }
-    });
+//     card.cardsClones.forEach((cardClonesNumber) => {
+//       const cardCloneLine = dataLines[cardClonesNumber - 1];
+//       const cardCloneParsed = parseCard(cardCloneLine, false);
+//       if (cardCloneParsed.cardsClones.length > 0) {
+//         cardsClones.push(cardCloneParsed);
+//       }
+//     });
 
-    if (card.cardsClones.length > 0 /* && cardsClones.length > 0 */) {
-      CARD_INDEXES.push(card.cardsClones);
-      return cardsClones;
-    }
+//     if (card.cardsClones.length > 0 /* && cardsClones.length > 0 */) {
+//       CARD_INDEXES.push(card.cardsClones);
+//       return cardsClones;
+//     }
 
-    return null;
-  });
+//     return null;
+//   });
 
-  clonesGroups.forEach((clonesGroup) => {
-    if (clonesGroup) {
-      recursiveCloneCards(clonesGroup, dataLines, CARD_INDEXES, isLoggingEnabled);
-    }
-  });
-};
+//   clonesGroups.forEach((clonesGroup) => {
+//     if (clonesGroup) {
+//       recursiveCloneCards(clonesGroup, dataLines, CARD_INDEXES, isLoggingEnabled);
+//     }
+//   });
+// };
 
-const getWinningCards = (dataLines, isLoggingEnabled) => {
+// const getWinningCards = (dataLines, isLoggingEnabled) => {
 
-  const CARD_INDEXES = [];
-  // Check if the input is a non-empty array
-  if (!Array.isArray(dataLines) || dataLines.length === 0) {
-    // If not, throw an error
-    throw new Error('Invalid input: dataLines must be a non-empty array');
-  }
+//   const CARD_INDEXES = [];
+//   // Check if the input is a non-empty array
+//   if (!Array.isArray(dataLines) || dataLines.length === 0) {
+//     // If not, throw an error
+//     throw new Error('Invalid input: dataLines must be a non-empty array');
+//   }
 
-  // Initialize the total sum to 0
-  let totalSum = 0;
-  let lineCount = 1;
-  const cards = [];
+//   // Initialize the total sum to 0
+//   let totalSum = 0;
+//   let lineCount = 1;
+//   const cards = [];
 
-  // Loop over each line in the dataLines array
-  for (const line of dataLines) {
-    const card = parseCard(line, isLoggingEnabled);
-    cards.push(card);
-    CARD_INDEXES.push(lineCount);
-    lineCount++;
-  }
+//   // Loop over each line in the dataLines array
+//   for (const line of dataLines) {
+//     const card = parseCard(line, isLoggingEnabled);
+//     cards.push(card);
+//     CARD_INDEXES.push(lineCount);
+//     lineCount++;
+//   }
 
-  cards.forEach((card) => {
-    totalSum += card.points;
-  });
+//   cards.forEach((card) => {
+//     totalSum += card.points;
+//   });
 
-  recursiveCloneCards(cards, dataLines, CARD_INDEXES, isLoggingEnabled);
+//   recursiveCloneCards(cards, dataLines, CARD_INDEXES, isLoggingEnabled);
 
-  const counts = CARD_INDEXES.flat().reduce((acc, index) => {
-    acc[index] = (acc[index] || 0) + 1;
-    return acc;
-  }, {});
+//   const counts = CARD_INDEXES.flat().reduce((acc, index) => {
+//     acc[index] = (acc[index] || 0) + 1;
+//     return acc;
+//   }, {});
 
-  const scratchcards = Object.entries(counts).map(([index, count]) => ({ index: Number(index), count }));
+//   const scratchcards = Object.entries(counts).map(([index, count]) => ({ index: Number(index), count }));
 
-  const scratchcardsObject = {
-    scratchcards,
-    total: scratchcards.reduce((acc, { count }) => acc + count, 0),
-  };
+//   const scratchcardsObject = {
+//     scratchcards,
+//     total: scratchcards.reduce((acc, { count }) => acc + count, 0),
+//   };
 
-  if (isLoggingEnabled) {
-    console.log('\n', scratchcardsObject);
-  }
+//   if (isLoggingEnabled) {
+//     console.log('\n', scratchcardsObject);
+//   }
 
-  return {
-    part1: totalSum,
-    part2: scratchcardsObject,
-  };
-};
+//   return {
+//     part1: totalSum,
+//     part2: scratchcardsObject,
+//   };
+// };
 
 /* MAIN EXECUTION
 ============================================================================= */
@@ -159,8 +161,15 @@ const [exampleData] = getRuntime(loadTextFile, `${process.cwd()}/src/days/${DAY}
 const [solutionSize] = getRuntime(getFileSize, `${process.cwd()}/src/days/${DAY}/solution.js`);
 
 // Calculate the solution for part 1 and measure the time it takes
-const [solution, solutionTime] = getRuntime(getWinningCards, dataLines, false);
+// const [solution, solutionTime] = getRuntime(getWinningCards, dataLines, false);
+// const [solution, solutionTime] = getRuntime(getSolution, exampleData, true);
 
+const solution = new Solution(exampleData, true);
+solution.calculateWinningCards();
+console.log('\n\n', solution);
+// console.log(solution.cards);
+// console.log('\npart-1:', solution.totalPoints);
+// console.log('part-2:', solution.totalScratchcards);
 
 /* OUTPUT LOGS
 ============================================================================= */
@@ -171,24 +180,24 @@ console.log('\n🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄\n');
 log(`${chalk.blue('Day:')} ${chalk.green(DAY[DAY.length - 1])}\n`);
 
 // Print the solutions for part 1 and part 2
-log(`${chalk.blue('Part-1:')} ${chalk.green(solution.part1)}`);
-log(`${chalk.blue('Part-2:')} ${chalk.green(solution.part2.total)}\n`);
+// log(`${chalk.blue('Part-1:')} ${chalk.green(solution.part1)}`);
+// log(`${chalk.blue('Part-2:')} ${chalk.green(solution.part2.total)}\n`);
 
 // Print the sizes of the script and data files
-console.log(`${chalk.blue('Script:')} ${chalk.green(`${solutionSize}kb`)}`);
-console.log(`${chalk.blue('Data:')} ${chalk.green(`${dataSize}kb\n`)}`);
+// console.log(`${chalk.blue('Script:')} ${chalk.green(`${solutionSize}kb`)}`);
+// console.log(`${chalk.blue('Data:')} ${chalk.green(`${dataSize}kb\n`)}`);
 
 // Print the total runtime
-console.log(`${chalk.blue('Runtime:')} ${chalk.green(`${solutionTime}`)}\n`);
+// console.log(`${chalk.blue('Runtime:')} ${chalk.green(`${solutionTime}`)}\n`);
 
 console.log('🎁🎁🎁🎁🎁🎁🎁🎁🎁🎁\n');
 
 // Print the header for the part 1 example
-console.log('Part 1 & 2\n');
-const [exampleSolution, exampleTime] = getRuntime(getWinningCards, exampleData, true);
-console.log('');
-console.log(`${chalk.blue('Total points:')} ${chalk.green(exampleSolution.part1)}`);
-console.log(`${chalk.blue('Total scratchcards:')} ${chalk.green(exampleSolution.part2.total)}`);
-console.log(`${chalk.blue('Runtime:')} ${chalk.green(`${exampleTime}`)}\n`);
+// console.log('Part 1 & 2\n');
+// const [exampleSolution, exampleTime] = getRuntime(getWinningCards, exampleData, true);
+// console.log('');
+// console.log(`${chalk.blue('Total points:')} ${chalk.green(exampleSolution.part1)}`);
+// console.log(`${chalk.blue('Total scratchcards:')} ${chalk.green(exampleSolution.part2.total)}`);
+// console.log(`${chalk.blue('Runtime:')} ${chalk.green(`${exampleTime}`)}\n`);
 
 console.log('🎅🎅🎅🎅🎅🎅🎅🎅🎅🎅\n');
