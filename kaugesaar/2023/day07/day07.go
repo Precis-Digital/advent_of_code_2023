2be87e1573419308e2f88097e1e284c797072524
Victor Kaugesaar <kaugesaar@precisdigital.com>
2023-12-07 13:50:06 +0100

    day 7 pt 1

---
+++ kaugesaar/2023/day07/day07.go
@@ -0,0 +1,165 @@
+package day07
+
+import (
+	_ "embed"
+	"fmt"
+	"kaugesaar-aoc/solution"
+	"kaugesaar-aoc/utils"
+	"sort"
+	"strings"
+)
+
+const (
+	fiveOfAKind  = 7
+	fourOfAKind  = 6
+	fullHouse    = 5
+	threeOfAKind = 4
+	twoPair      = 3
+	pair         = 2
+	nothing      = 1
+)
+
+// Solver for day 7 and its both parts
+type Solver struct{}
+
+//go:embed day7.txt
+var fileInput string
+
+func cardIndex(c rune) int {
+	switch c {
+	case 'A':
+		return 14
+	case 'K':
+		return 13
+	case 'Q':
+		return 12
+	case 'J':
+		return 11
+	case 'T':
+		return 10
+	default:
+		return int(c - '0')
+	}
+}
+
+func getHandType(counts []int) int {
+	max := 0
+	for _, count := range counts {
+		if count > max {
+			max = count
+		}
+	}
+
+	switch {
+	case max == 5:
+		return fiveOfAKind
+
+	case max == 4:
+		return fourOfAKind
+
+	case max == 3:
+		for _, count := range counts {
+			if count == 2 {
+				return fullHouse
+			}
+		}
+		return threeOfAKind
+
+	case max == 2:
+		pairs := 0
+		for _, count := range counts {
+			if count == 2 {
+				pairs++
+			}
+		}
+		switch {
+		case pairs == 2:
+			return twoPair
+		default:
+			return pair
+		}
+	case max == 1:
+		return nothing
+	default:
+		panic(fmt.Sprintf("%v", counts))
+	}
+}
+
+func handStrength(cards string) (int, int) {
+	countsByCard := make(map[rune]int)
+	for _, c := range cards {
+		countsByCard[c]++
+	}
+
+	counts := make([]int, 0, len(countsByCard))
+	for _, v := range countsByCard {
+		counts = append(counts, v)
+	}
+
+	idx := 0
+	for _, c := range cards {
+		idx = (idx << 4) + cardIndex(c)
+	}
+
+	return getHandType(counts), idx
+}
+
+func parser() [][]int {
+	rows := strings.Split(fileInput, "\n")
+	hands := make([][]int, 0)
+	for _, row := range rows {
+		if row == "" {
+			continue
+		}
+
+		hand := make([]int, 3)
+		parts := strings.Split(row, " ")
+
+		hand[0], hand[1] = handStrength(parts[0])
+		hand[2] = utils.ToInt(parts[1])
+		hands = append(hands, hand)
+	}
+
+	// Sorting the hands slice
+	sort.Slice(hands, func(i, j int) bool {
+		if hands[i][0] == hands[j][0] {
+			return hands[i][1] < hands[j][1]
+		}
+		return hands[i][0] < hands[j][0]
+	})
+
+	return hands
+}
+
+func p1() string {
+	sum := 0
+	hands := parser()
+
+	for i, hand := range hands {
+		sum += hand[2] * (i + 1)
+	}
+
+	return utils.ToStr(sum)
+}
+
+func p2() string {
+	return utils.ToStr(2)
+}
+
+// Part1 the solution for part 1, day 7
+func (s Solver) Part1() solution.Response {
+	return solution.Response{
+		Day:    7,
+		Part:   1,
+		Answer: p1(),
+	}
+}
+
+// Part2 the solution for part 2, day 7
+func (s Solver) Part2() solution.Response {
+	return solution.Response{
+		Day:    7,
+		Part:   2,
+		Answer: p2(),
+	}
+}
