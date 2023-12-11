bbc5f4fe0a98987d5a07cd511df1f8cfbe4b48e5
Victor Kaugesaar <kaugesaar@precisdigital.com>
2023-12-11 16:44:35 +0100

    day 11 bench

--- kaugesaar/2023/day11/day11_test.go
+++ kaugesaar/2023/day11/day11_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPart1(t *testing.T) {
 	result := p1()
-	expected := ""
+	expected := "9522407"
 	if result != expected {
 		t.Errorf("Result is incorrect, got %s, wanted: %s", result, expected)
 	}
@@ -12,7 +12,7 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	result := p2()
-	expected := ""
+	expected := "544723432977"
 	if result != expected {
 		t.Errorf("Result is incorrect, got %s, wanted: %s", result, expected)
 	}
